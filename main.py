6eec3777c9ae8964bfcb5b014f710e783cf71a4d
PabloEskabarro <nonzxckid76@gmail.com>
2025-01-23 20:39:56 +0300

    Update - 2 Загрузка карты и камера перенесены в отдельный файл

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ import subprocess
 
 from settings import all_sprites, platforms_sprites
 
-from objects.entity.hero import Hero
-from objects.entity.wolf import Wolf
-
-from objects.stucturies.platforms import Platform
-from objects.stucturies.platforms import Platform_ver
-from objects.stucturies.grass import Ground
-
 from settings import screen_height, screen_width
 
+from map_loader import load_map_from_file, Camera
 
 def start_game():
     subprocess.run(['python', 'settings.py'])
@@ -32,105 +26,9 @@ pygame.display.set_caption("Echo's Journey")
 background_image = pygame.image.load("textures/forest_background.png")
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))  # Масштабируем фон под экран
 
-# Загрузка текстуры травы
-ground_image = pygame.image.load("textures/ground.png")
-
-# Словарь для размеров платформ
-platform_sizes = {
-    's': (0.1, 0.02),  # Маленькая платформа (длинна платформы, толщина платформы)
-    'm': (0.2, 0.021),  # Средняя платформа (длинна платформы, толщина платформы)
-    'b': (0.3, 0.02),  # Большая платформа (длинна платформы, толщина платформы)
-}
-
-ver_platform_sizes = {
-    'v': (0.06, 0.02),  # Маленькая платформа (длинна платформы, толщина платформы)
-}
-
-
-def load_map_from_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            global cell_width
-
-            map_data = [line.strip() for line in file]
-
-            # Определяем размеры одной ячейки карты в пикселях
-            cell_width = screen_width / len(map_data[0])  # Ширина одной ячейки
-
-            vertical_spacing = -0.01
-            platform_count = 0  # Счетчик для уникальных имен платформ
-
-            for y, row in enumerate(map_data):
-                for x, cell in enumerate(row):
-                    # Вычисляем координаты верхнего левого угла ячейки
-                    world_x = x * cell_width
-
-                    if cell == 'g':
-                        ground = Ground((world_x, world_y), ground_image)
-                        all_sprites.add(ground)
-
-                    if cell in platform_sizes:
-                        width_percent, height_percent = platform_sizes[cell]
-                        platform_width = int(screen_width * width_percent)
-                        platform_height = int(screen_height * height_percent)
-
-                        # Добавляем контролируемый отступ между строками
-                        world_y = y * (platform_height + vertical_spacing)
-
-                        platform = Platform(world_x, world_y, platform_width, platform_height)
-                        platforms_sprites.add(platform)
-                        all_sprites.add(platform)
-
-                        # Генерируем уникальное имя для платформы
-                        platform_type = cell
-                        platform_name = f"{platform_type}_platform_{platform_count}"
-                        platform_count += 1
-
-                    if cell in ver_platform_sizes:
-                        width_percent, height_percent = ver_platform_sizes[cell]
-                        platform_width = int(screen_width * width_percent)
-                        platform_height = int(screen_height * height_percent)
-
-                        # Добавляем контролируемый отступ между строками
-                        world_y = y * (platform_height + vertical_spacing)
-
-                        ver_platform = Platform_ver(world_x, world_y, platform_height, platform_width)
-                        # platforms_sprites.add(ver_platform)
-                        all_sprites.add(ver_platform)
-
-                    elif cell == '@':  # Игрок
-                        global player
-                        player = Hero(world_x, world_y)
-                        all_sprites.add(player)
-
-                    elif cell == 'w':  # Волк
-                        wolf = Wolf(world_x, world_y)
-                        all_sprites.add(wolf)
-
-        return map_data, player, cell_width
-    except FileNotFoundError:
-        print(f"Файл {filename} не найден!")
-        sys.exit()
-
-
 # Загружаем карту
 map_file = "LvL1.map"
-map_data = load_map_from_file(map_file)
-
-
-class Camera:
-    def __init__(self):
-        self.dx = 0  # Горизонтальное смещение
-        self.dy = 0  # Вертикальное смещение
-
-    def apply(self, obj):
-        obj.rect.x += self.dx
-
-
-    def update(self, target):
-        self.dx = -(target.rect.centerx - screen_width // 2)
-
-
+map_data, player = load_map_from_file(map_file)
 
 # Главный игровой цикл
 clock = pygame.time.Clock()
