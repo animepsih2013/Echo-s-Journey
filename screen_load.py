718fbe8737cfee05fd61a421414aa5bcd9df840e
KiAMurrA <sergey.uin.7@gmail.com>
2025-02-01 16:11:17 +0300

    screen_load finished

--- screen_load.py
+++ screen_load.py
@@ -1,42 +1,40 @@
 import pygame
 import subprocess
 import sys
-screen_height, screen_width = 1000, 1000
+
+pygame.init()
 FPS = 50
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((950, 633))
 clock = pygame.time.Clock()
 
-
+fon = pygame.transform.scale(pygame.image.load('textures/sc.png'), (950, 633))
 def terminate():
     pygame.quit()
     sys.exit()
 
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+screen.blit(fon, (0, 0))
+intro_text = ["ЗАСТАВКА", "",
+              "Правила игры",
+              "Если в правилах несколько строк,",
+              "приходится выводить их построчно"]
 
-    fon = pygame.transform.scale(pygame.image.load('textures/sc.png'), (screen_width, screen_height))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+font = pygame.font.Font(None, 30)
+text_coord = 50
+for line in intro_text:
+    string_rendered = font.render(line, 1, pygame.Color('black'))
+    intro_rect = string_rendered.get_rect()
+    text_coord += 10
+    intro_rect.top = text_coord
+    intro_rect.x = 10
+    text_coord += intro_rect.height
+    screen.blit(string_rendered, intro_rect)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return subprocess.run(['python', 'main.py'])
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            subprocess.run(['python', 'main.py'])
 
-        pygame.display.flip()
-        clock.tick(FPS)
+    pygame.display.flip()
+    clock.tick(FPS)
